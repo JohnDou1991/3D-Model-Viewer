705dea2905181cdb35f42728a1cf1d37d0fcb77f
OSennikov <yoctoadm@kickseed.localdomain>
2021-02-12 15:51:10 +0000

    Add glfw, glad and glew as a sub-projects. Slightly change namings in CMakeLists files. Tested on Linux

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,32 +1,41 @@
+#include "iostream"
+
 #if defined(_WIN64) || defined(_WIN32)
     #define GLUT_DISABLE_ATEXIT_HACK
     #define FREEGLUT_STATIC
+    #define GLEW_STATIC
     #include "GL/freeglut.h"
 #elif __linux__
     #include "GL/glut.h"
-    #include "GL/freeglut_ext.h"
 #endif
 
 using namespace std;
 
-static void display(void)
-{
-    glutSwapBuffers();
+/* Handler for window-repaint event. Call back when the window first appears and
+   whenever the window needs to be re-painted. */
+void display() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+ 
+   // Draw a Red 1x1 Square centered at origin
+   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+      glColor3f(1.0f, 0.0f, 0.0f); // Red
+      glVertex2f(-0.5f, -0.5f);    // x, y
+      glVertex2f( 0.5f, -0.5f);
+      glVertex2f( 0.5f,  0.5f);
+      glVertex2f(-0.5f,  0.5f);
+   glEnd();
+ 
+   glFlush();  // Render now
 }
-
-int main( int argc, char* argv[])
-{
-    glutInitWindowSize(800,600);
-    glutInitWindowPosition(40,40);
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
-    
-    glutCreateWindow( "PROJECTX" );
-
-    glutDisplayFunc(display);
-
-    glutSetOption( GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION );
-    
-    glutMainLoop();
-    return 0;
+ 
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);                 // Initialize GLUT
+   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+   glutInitWindowSize(320, 320);   // Set the window's initial width & height
+   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutMainLoop();           // Enter the infinitely event-processing loop
+   return 0;
 }
