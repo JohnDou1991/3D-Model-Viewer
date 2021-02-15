03b31d473ac529107aea3c649f280a42712a72ac
OSennikov <yoctoadm@kickseed.localdomain>
2021-02-15 15:51:26 +0000

    [LEARNING][LINUX]Shader example implementation.

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,41 +1,161 @@
-#include "iostream"
-
 #if defined(_WIN64) || defined(_WIN32)
     #define GLUT_DISABLE_ATEXIT_HACK
     #define FREEGLUT_STATIC
     #define GLEW_STATIC
     #include "GL/freeglut.h"
 #elif __linux__
-    #include "GL/glut.h"
+   #include "GL/glew.h"
+   #include "GLFW/glfw3.h"
 #endif
+
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <map>
+#include <string>
+
 
 using namespace std;
 
-/* Handler for window-repaint event. Call back when the window first appears and
-   whenever the window needs to be re-painted. */
-void display() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
- 
-   // Draw a Red 1x1 Square centered at origin
-   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-      glColor3f(1.0f, 0.0f, 0.0f); // Red
-      glVertex2f(-0.5f, -0.5f);    // x, y
-      glVertex2f( 0.5f, -0.5f);
-      glVertex2f( 0.5f,  0.5f);
-      glVertex2f(-0.5f,  0.5f);
-   glEnd();
- 
-   glFlush();  // Render now
+GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
+GLuint VBO;
+GLuint VAO;
+GLuint shaderProgram;
+
+using Name = string;
+
+struct Shader{
+   GLuint shader;
+   string file;
+   const GLchar ** source;
+};
+
+std::map<Name,Shader> shaderPool;
+
+const std::string readFile( const std::string& path )
+{
+   std::string result;
+   std::ifstream t( path.c_str() );
+
+   t.seekg(0, std::ios::end);   
+   result.reserve(t.tellg());
+   t.seekg(0, std::ios::beg);
+
+   result.assign(( std::istreambuf_iterator<char>(t) ), std::istreambuf_iterator<char>() );
+   return result;
 }
- 
+
+Shader readShader( const std::string& path, GLenum type )
+{
+   Shader result;
+   GLuint shader = glCreateShader(type);
+   result.shader = shader;
+   result.file = readFile( path );
+   return result;
+}
+
+bool compileShader( Shader& shader )
+{
+   const GLchar* source = shader.file.c_str();
+   glShaderSource(shader.shader, 1, &source, NULL);
+   glCompileShader(shader.shader);
+
+   GLint success;
+   GLchar infoLog[512];
+   glGetShaderiv(shader.shader, GL_COMPILE_STATUS, &success);
+
+   if(!success)
+   {
+      glGetShaderInfoLog(shader.shader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+   }
+
+   return success;
+}
+
+void loadShaders()
+{
+   shaderPool["vertex"] = std::move(readShader( "/home/yoctoadm/Desktop/Git/projectX/sources/shaders/vertex_shader", GL_VERTEX_SHADER ));
+   if ( !compileShader(shaderPool["vertex"]) ) return;
+
+   shaderPool["fragment"] = readShader( "/home/yoctoadm/Desktop/Git/projectX/sources/shaders/fragment_shader", GL_FRAGMENT_SHADER );
+   if ( !compileShader(shaderPool["fragment"]) ) return;
+
+   shaderProgram = glCreateProgram();
+
+   glAttachShader(shaderProgram, shaderPool["vertex"].shader);
+   glAttachShader(shaderProgram, shaderPool["fragment"].shader);
+   glLinkProgram(shaderProgram);
+   glUseProgram(shaderProgram);
+
+   glDeleteShader(shaderPool["vertex"].shader);
+   glDeleteShader(shaderPool["fragment"].shader);
+}
+
+void init( )
+{
+   glGenBuffers(1, &VBO);
+   glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+   glGenVertexArrays(1, &VAO);
+   glBindVertexArray(VAO);
+
+   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   
+   glEnableVertexAttribArray(0);
+   glBindBuffer(GL_ARRAY_BUFFER, 0);
+   glBindVertexArray(0);
+}
+
 /* Main function: GLUT runs as a console application starting at main()  */
-int main(int argc, char** argv) {
-   glutInit(&argc, argv);                 // Initialize GLUT
-   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
-   glutInitWindowSize(320, 320);   // Set the window's initial width & height
-   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-   glutDisplayFunc(display); // Register display callback handler for window re-paint
-   glutMainLoop();           // Enter the infinitely event-processing loop
+int main(int argc, char** argv) 
+{
+   if(!glfwInit()){
+      return -1;
+   }
+
+   // Create a GLFWwindow object that we can use for GLFW's functions
+   GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+   glfwMakeContextCurrent(window);
+
+   // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+   glewExperimental = GL_TRUE;
+   // Initialize GLEW to setup the OpenGL Function pointers
+   glewInit();
+
+   // Define the viewport dimensions
+   int width, height;
+   glfwGetFramebufferSize(window, &width, &height);  
+   glViewport(0, 0, width, height);
+
+   loadShaders();
+   init();
+
+   while (!glfwWindowShouldClose(window))
+   {
+      // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+      glfwPollEvents();
+
+      // Render
+      // Clear the colorbuffer
+      glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      // Draw our first triangle
+      glUseProgram(shaderProgram);
+      glBindVertexArray(VAO);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glBindVertexArray(0);
+
+      // Swap the screen buffers
+      glfwSwapBuffers(window);
+   }
+
    return 0;
 }
